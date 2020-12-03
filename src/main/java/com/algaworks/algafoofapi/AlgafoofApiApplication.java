070a7788351318dc0e08a54ba1e04924b8b655ba
Alexandre Tannus <alexandretannus@gmail.com>
2020-12-02 23:50:30 -0300

    Criação do projeto Spring

---
+++ src/main/java/com/algaworks/algafoofapi/AlgafoofApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoofapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoofApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoofApiApplication.class, args);
+	}
+
+}
